4c0f2aa2ad3ffb103a3901205a8fd41a2eb883cf
SilmiNawaz <snawaz@ewise.com>
2016-09-03 10:59:56 +0800

    add pdv and pfm data objects

--- app/src/main/java/com/ewise/moneyapp/dataobjects/CurrencyExchangeRateItem.java
+++ app/src/main/java/com/ewise/moneyapp/dataobjects/CurrencyExchangeRateItem.java
@@ -1,20 +1,89 @@
 package com.ewise.moneyapp.dataobjects;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 /**
  * Created by SilmiNawaz on 28/8/16.
  */
 public class CurrencyExchangeRateItem {
-    public Currency buyCurrency;
-    public Currency sellCurrency;
-    public BigDecimal exchangeRate;
+    public String baseCurrencyCode; //e.g. SGD       USD           USD         SGD
+    public String quoteCurrencyCode;//e.g. USD       SGD           SGD         USD
+    public BigDecimal exchangeRate; //e.g. 0.7357    1.3592        0.7357      1.3592
+    public boolean rateQuoteDirect; //e.g. true      true          false       false
 
-    public CurrencyExchangeRateItem(Currency buyCurrency, Currency sellCurrency, BigDecimal exchangeRate){
-        this.buyCurrency = buyCurrency;
-        this.sellCurrency = sellCurrency;
+    public CurrencyExchangeRateItem(String baseCurrencyCode,
+                                    String quoteCurrencyCode,
+                                    boolean rateQuoteDirect,
+                                    BigDecimal exchangeRate){
+
+        this.baseCurrencyCode = baseCurrencyCode;
+        this.quoteCurrencyCode = quoteCurrencyCode;
         this.exchangeRate = exchangeRate;
+        this.rateQuoteDirect = rateQuoteDirect;
     }
 
+    public boolean RateIsValidForCurrency (String validCurrencyCode){
+
+        if (validCurrencyCode.equals(this.baseCurrencyCode)){
+            return true;
+        }
+        else {
+            if (validCurrencyCode.equals(this.quoteCurrencyCode)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public BigDecimal ExchangeToQuoteCurrency (BigDecimal baseCurrencyAmount){
+
+        BigDecimal quoteCurrencyAmount;
+        Currency baseCurrency = Currency.getInstance(this.baseCurrencyCode);
+        Currency quoteCurrency = Currency.getInstance(this.quoteCurrencyCode);
+        int precision = 10;
+        precision = precision + quoteCurrency.getDefaultFractionDigits(); //make sure we support the fractions
+
+
+        if (rateQuoteDirect){
+            //determine the precision including fractions
+            int scale = baseCurrency.getDefaultFractionDigits() + exchangeRate.scale();
+            precision = precision + scale;
+
+            quoteCurrencyAmount = baseCurrencyAmount.multiply(exchangeRate, new MathContext(precision, RoundingMode.HALF_UP));
+            quoteCurrencyAmount = quoteCurrencyAmount.setScale(quoteCurrency.getDefaultFractionDigits(), RoundingMode.HALF_UP);
+        }
+        else {
+            quoteCurrencyAmount = baseCurrencyAmount.divide(exchangeRate,quoteCurrency.getDefaultFractionDigits(), RoundingMode.HALF_UP);
+        }
+
+        return quoteCurrencyAmount;
+
+    }
+
+    public BigDecimal ExchangeToBaseCurrency (BigDecimal quoteCurrencyAmount){
+        BigDecimal baseCurrencyAmount;
+        Currency baseCurrency = Currency.getInstance(this.baseCurrencyCode);
+        Currency quoteCurrency = Currency.getInstance(this.quoteCurrencyCode);
+        int precision = 10;
+        precision = precision + baseCurrency.getDefaultFractionDigits(); //make sure we support the fractions
+
+        if (rateQuoteDirect){
+            baseCurrencyAmount = quoteCurrencyAmount.divide(exchangeRate,baseCurrency.getDefaultFractionDigits(), RoundingMode.HALF_UP);
+
+        }
+        else {
+            int scale = baseCurrency.getDefaultFractionDigits() + exchangeRate.scale();
+            precision = precision + scale;
+
+            baseCurrencyAmount = quoteCurrencyAmount.multiply(exchangeRate, new MathContext(precision, RoundingMode.HALF_UP));
+            baseCurrencyAmount = baseCurrencyAmount.setScale(baseCurrency.getDefaultFractionDigits(), RoundingMode.HALF_UP);
+
+        }
+
+        return baseCurrencyAmount;
+    }
 }
--- app/src/main/java/com/ewise/moneyapp/dataobjects/CurrencyExchangeRates.java
+++ app/src/main/java/com/ewise/moneyapp/dataobjects/CurrencyExchangeRates.java
@@ -1,9 +1,42 @@
 package com.ewise.moneyapp.dataobjects;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
 /**
  * Created by SilmiNawaz on 28/8/16.
  */
 public class CurrencyExchangeRates {
+
+
+    public final static CurrencyExchangeRates INSTANCE = new CurrencyExchangeRates();
+
+    private ArrayList<CurrencyExchangeRateItem> rateItemArrayList = new ArrayList<CurrencyExchangeRateItem>();
+
+    protected CurrencyExchangeRates() {
+        // Exists only to defeat instantiation.
+    }
+
+    //load rates
+    public int loadExchangeRates () {
+
+        //todo: get currency rates from data source
+
+
+        //todo: temp: dummy rates load
+        BigDecimal rate = new BigDecimal("1.0");
+        CurrencyExchangeRateItem rateItem = new CurrencyExchangeRateItem(Currency.getInstance("SGD"), Currency.getInstance("SGD"), true, rate);
+
+        return 0;
+
+    }
+
+
+    public CurrencyExchangeRateItem getCurrencyRatesForCurrencyPair (String baseCurrencyCode, String quoteCurrencyCode){
+        for (CurrencyExchangeRateItem rateItem : this.rateItemArrayList) {
+
+        }
+    }
 
 
 }
